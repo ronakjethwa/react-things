94fcd5d0662d835069648e20852a875b539b9fc6
Ronak Jethwa <ronakjethwa@ronakjethwa.local>
2020-06-19 00:44:36 -0400

    form component

--- address-book/src/App.js
+++ address-book/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import './Form';
+import Form from './Form'
 class App extends Component{
   
   constructor(props){
--- address-book/src/Form.js
+++ address-book/src/Form.js
@@ -15,36 +15,64 @@ class Form extends Component {
   }
 
   handleChange = (event) => {
-    const { name, addressline, city, country } = event.target;
+    const { name, value } = event.target;
 
+    this.setState({
+      [name]: value,
+    });
   };
 
   onFormSubmit = (event) => {
     event.preventDefault();
-
+    
     this.props.handleSubmit(this.state);
     this.setState(this.initialState);
   };
 
   render() {
-
     const { name, addressline, city, country } = this.state;
-    return(
-      <form>
+    return (
+      <form onSubmit={this.onFormSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" value={name}></input>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={this.handleChange}
+        ></input>
 
         <label htmlFor="addressline">Address Line</label>
-        <input type="text" name="adddressline" id="addressline" value={addressline}></input>
+        <input
+          type="text"
+          name="addressline"
+          id="addressline"
+          value={addressline}
+          onChange={this.handleChange}
+        ></input>
 
         <label htmlFor="city">City</label>
-        <input type="text" name="city" id="city" value={city}></input>
+        <input
+          type="text"
+          name="city"
+          id="city"
+          value={city}
+          onChange={this.handleChange}
+        ></input>
 
         <label htmlFor="country">Country</label>
-        <input type="text" name="country" id="country" value={country}></input>
+        <input
+          type="text"
+          name="country"
+          id="country"
+          value={country}
+          onChange={this.handleChange}
+        ></input>
 
+        <br />
+        <button type="submit">Submit</button>
       </form>
-    )
+    );
   }
 }
 
