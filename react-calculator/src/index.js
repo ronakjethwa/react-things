f8653ecde09814d7b06ffb276ad2fe176858a429
Ronak Jethwa <ronakjethwa@ronakjethwa.local>
2020-07-17 20:32:50 -0400

    initial

---
+++ react-calculator/src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return null;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
