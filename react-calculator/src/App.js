085a0bcda6efa592ef52d1d29795b13ba82ede32
Ronak Jethwa <ronakjethwa@ronakjethwa.local>
2020-07-17 21:37:16 -0400

    eslint

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -36,6 +36,7 @@ class App extends Component {
 
     try {
       this.setState({
+        // eslint-disable-next-line
         result: (eval(checkResult) || "") + "",
       });
     } catch (e) {
