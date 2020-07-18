1ddbb388d17caed19641f70d7459b011085e0cc9
Ronak Jethwa <ronakjethwa@ronakjethwa.local>
2020-07-17 21:55:21 -0400

    updates

--- react-calculator/src/components/KeyPad.js
+++ react-calculator/src/components/KeyPad.js
@@ -4,8 +4,8 @@ class KeyPad extends Component {
   render() {
     return (
       <div className="button">
-        <button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
-        <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
+        <button name="" onClick={e => this.props.onClick(e.target.name)}>&nbsp;</button>
+        <button name="" onClick={e => this.props.onClick(e.target.name)}>&nbsp;</button>
         <button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
         <button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button>
 
